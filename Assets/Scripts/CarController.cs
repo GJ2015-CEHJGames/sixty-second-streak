614120d6e7c538805843117e7fc652202b209ab7
Chris Chung <chrixeleon@gmail.com>
2015-01-25 13:42:02 -0500

    Added new car sprites. Made new build (1:41PM).

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CarController : MonoBehaviour
 {
-	public int dir; // -1 = left, 1 = right
+	public int dir; // 1 = left, -1 = right
 	public int speed = 200;
 	private Vector2 escapeVector;
 	private bool wasHit = false;
@@ -12,16 +13,26 @@ public class CarController : MonoBehaviour
 	public AudioClip honkSound;
 	public AudioClip crashSound;
 	private float honkTimer;
+	public List<Color> carColors;
 
 	// Use this for initialization
 	void Start()
 	{
 		honkTimer = Random.Range(7.0f, 15.0f);
 	}
-	
+
+	void Awake()
+	{
+		gameObject.GetComponent<SpriteRenderer>().color = carColors[Random.Range(0, carColors.Count)];
+	}
+
 	// Update is called once per frame
 	void Update()
 	{
+		Vector3 tempScale = transform.localScale;
+		tempScale.x = -dir;
+		transform.localScale = tempScale;
+
 		Vector2 vel = rigidbody2D.velocity;
 
 		vel.x = dir * speed * Time.deltaTime;
